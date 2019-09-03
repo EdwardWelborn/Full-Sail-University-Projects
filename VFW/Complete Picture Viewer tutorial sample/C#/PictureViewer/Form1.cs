7e924b0ae82d8f5bfba4c0818d63ead966ef5625
Roy Welborn <erwelborn@student.fullsail.edu>
2019-09-03 17:14:07 -0500

    Adding Files for VFW
    Adding files for VFW class as well as some changes to IUS

---
+++ VFW/Complete Picture Viewer tutorial sample/C#/PictureViewer/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            // No code needed here for this sample.
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Show the Open File dialog. If the user chooses OK, load the 
+            // picture that the user chose.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the picture.
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            // Show the color picker dialog box. If the user chooses OK, change the 
+            // PictureBox control's background to the color the user chose. 
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Close the form. 
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // If the user selects the Stretch check box,  
+            // change the PictureBox's SizeMode property to "Stretch". 
+            // If the user clears the check box, change it to "Normal".
+            // Choosing Stretch shows the entire image in the available space.          
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+    }
+}
---
+++ VFW/WindowsFormsIntro/WindowsFormsIntro/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsIntro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ExitApplication(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void defaultButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Default";
+            checkboxStudent.Checked = false;
+        }
+    }
+}
