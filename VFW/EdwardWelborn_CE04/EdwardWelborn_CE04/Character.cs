db5c47478bf11a8202c317ad63917cad8e164a94
Roy Welborn <erwelborn@student.fullsail.edu>
2019-09-12 10:17:09 -0500

    CE04 Changes
    Added character class to the project, and slowly incorporating that changes to the end product

---
+++ VFW/EdwardWelborn_CE04/EdwardWelborn_CE04/Character.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdwardWelborn_CE04
+{
+    class Character
+    {
+        public string Name { get; set; }
+        public string Gender{ get; set; }
+        public string Level { get; set; }
+        public string Class { get; set; }
+        public string Race { get; set; }
+        public string Role { get; set; }
+        public bool Mentor { get; set; }
+        public override string ToString()
+        {
+            return Class + ": " + Name;
+        }
+    }
+}
