74afd4c646a41ac0c89670a5f0190024faf48341
Edward Roy Welborn <erwelborn@student.fullsail.edu>
2022-01-03 18:06:19 -0600

    Made changes to first part of the Lab 2 program, finished the first part, now on to the second

--- PG1/Lab2/Lab 2/Lab 2/Submission.cs
+++ PG1/Lab2/Lab 2/Lab 2/Submission.cs
@@ -20,56 +20,72 @@ namespace FSPG1
         // to an integer. Return the integer
         public static int Test3(string input)
         {
-            return 0;
+            int intNumber;
+            intNumber = int.Parse(input);
+            return intNumber;
         }
 
         // Given a string, using the SByte class, convert the string 
         // to a signed byte. Return the signed byte
         public static sbyte Test4(string input)
         {
-            return 0;
+            sbyte sByteNumber;
+            sByteNumber = sbyte.Parse(input);
+            return sByteNumber;
         }
 
         // Given a string, using the Double class, convert the string 
         // to a double Return the double
         public static double Test5(string input)
         {
-            return 0;
+            double dblNumber;
+            dblNumber = double.Parse(input);
+            return dblNumber;
         }
 
         // Given a string, using the UInt16 class, convert the string
         // to a unsigned short. Return the unsigned short
         public static ushort Test6(string input)
         {
-            return 0;
+            ushort uShortNumber;
+            uShortNumber = ushort.Parse(input);
+            return uShortNumber;
         }
 
         // Given a string, using the Single class, convert the string 
         // to a float. Return the float
         public static float Test7(string input)
         {
-            return 0;
+            float floatNumber;
+            floatNumber = float.Parse(input);
+            return floatNumber;
         }
 
         // Given a string, using the UInt32 class, convert the string 
         // to an unsigned integer. Return the unsigned integer
         public static uint Test8(string input)
         {
-            return 0;
+            uint uintNumber;
+            uintNumber = uint.Parse(input);
+            return uintNumber;
         }
 
         // Given a string, using the Int16 class, convert the string 
         // to a short. Return the short
         public static short Test9(string input)
         {
-            return 0;
+            short shortNumber;
+            shortNumber = short.Parse(input);
+            return shortNumber;
         }
 
         // Given a string, using the UInt64 class, convert the string 
         // to an unsigned long. Return the unsigned long
         public static ulong Test10(string input)
         {
-            return 0;
+            ulong uLongNumber;
+            uLongNumber = ulong.Parse(input);
+            return uLongNumber;
         }
     }
 }
