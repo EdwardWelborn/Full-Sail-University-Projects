9c1bf471a60fb2aad96bea00bf936b2320843b54
Edward Roy Welborn <erwelborn@student.fullsail.edu>
2022-03-02 13:09:46 -0600

    Completed Lab1, tweaked a few settings to make it look better

--- SDV3111-0/Lab1/Lab1/Lab1/Lab1.cpp
+++ SDV3111-0/Lab1/Lab1/Lab1/Lab1.cpp
@@ -19,16 +19,16 @@ void Program10();
 
 int main()
 {
-//    Program1(); // Done
-//    Program2(); // Done
-//    Program3(); // Done
-//    Program4(); // Done
-//    Program5(); // Done
-    Program6();
-//    Program7();
-//    Program8();
-//    Program9();
-//    Program10();
+    Program1(); // Done
+    Program2(); // Done
+    Program3(); // Done
+    Program4(); // Done
+    Program5(); // Done
+    Program6(); // Done
+    Program7(); // Done
+    Program8(); // Done
+    Program9(); // Done
+    Program10();// Done
 }
 
 // Clear Buffer and Validation Methods
@@ -64,7 +64,7 @@ void Program1()
     age = ValidateInt(age);
     age *= 365;
     std::cout << initials << ", you are a total of " << age << " days old? \n";
-    ClearInputBuffer();
+
     std::cout << "\n";
 }
 //Program 1-2
@@ -75,16 +75,22 @@ void Program2()
     
     for (int x = 0; x < 5; x++)
     {
-        std::cout << "Please Enter a number: ";
+        std::cout << "Please Enter a number: (" << x + 1 << " of 5) ";
         integers[x] = ValidateInt(integers[x]);
     }
     std::cout << "The Numbers you entered are:  ";
     for (int x = 0; x < 5; x++)
     {
-        std::cout << integers[x];
+        if (x < 4)
+        {
+            std::cout << integers[x] << ",";
+        }
+        else
+        {
+            std::cout << integers[x];
+        }
     }
-    ClearInputBuffer();
-    std::cout << "\n";
+    std::cout << "\n\n";
 }
 
 // Program 1-3
@@ -96,11 +102,11 @@ void Program3()
     int number3 = 0;
     int results = 0;
 
-    std::cout << "Please enter a valid number for number 1:";
+    std::cout << "Please enter a valid number for number 1: ";
     number1 = ValidateInt(number1);
-    std::cout << "Please enter a valid number for number 2:";
+    std::cout << "Please enter a valid number for number 2: ";
     number2 = ValidateInt(number2);
-    std::cout << "Please enter a valid number for number 1:";
+    std::cout << "Please enter a valid number for number 1: ";
     number3 = ValidateInt(number3);
 
     results = number1 + 1 * number2 + 2 - number3;
@@ -108,7 +114,6 @@ void Program3()
     results = (number1 + 1) * (number2 + 2) - number3;
     std::cout << "(a + 1) * (b + 2) - c = " << results;
 
-    ClearInputBuffer();
     std::cout << "\n";
 }
 
@@ -125,7 +130,6 @@ void Program4()
     std::cout << "\nlonglong \t" << LLONG_MIN << " - " << LLONG_MAX;
     std::cout << "\nulonglong \t 0 - " << ULONG_MAX;
 
-    ClearInputBuffer();
     std::cout << "\n";
 }
 
@@ -139,7 +143,6 @@ void Program5()
 	std::cin.getline(name, 32);
     std::cout << "Greetings " << name << ", did you know, that the dot over the i is called a title?";
 
-    ClearInputBuffer();
     std::cout << "\n";
 }
 
@@ -150,9 +153,192 @@ void Program6()
 
     int age = 0;
 
+    std::cout << "\nPlease enter your age: ";
+    age = ValidateInt(age);
 
+    if (age < 16)
+    {
+        std::cout << "\nYou are too young to be playing this game.";
+    }
+    else if (age >= 16 && age < 30)
+    {
+        std::cout << "\nYou are in the prime of your life, enjoy it!";
+    }
+    else if (age >= 30 && age < 45)
+    {
+        std::cout << "\nYou are in the middle of your life, go forth and have fun";
+    }
+    else if (age >= 45 && age < 65)
+    {
+        std::cout << "\nWork hard and save for your retirement";
+    }
+    else if (age > 65)
+    {
+        std::cout << "\nAWESOME! Travel time, enjoy your golden years.";
+    }
+    std::cout << "\n";
+}
 
+// Program 1-7
+void Program7()
+{
+    std::cout << "==================== Program 1-7 ====================\n";
+    int number = 0;
+
+    std::cout << "\nPlease enter a number to check: ";
+    number = ValidateInt(number);
+
+    if (number % 2 == 0)
+    {
+        std::cout << "\nRight on! The number is even.";
+    }
+    else
+    {
+        std::cout << "\nNo sir, This number is NOT even.";
+    }
+
+    std::cout << "\n";
 
 }
+
+// Program 1-8
+void Program8()
+{
+    std::cout << "==================== Program 1-8 ====================\n";
+    int number = 0;
+    int rndNumber = 0;
+    srand(time(0));
+
+    std::cout << "\nPlease enter a divisor to check: ";
+    number = ValidateInt(number);
+
+    for (int i = 1; i <=3; i++)
+    {
+        rndNumber = rand();
+        if (rndNumber % number == 0)
+        {
+            std::cout << "\nRandom Number: \t" << rndNumber << "\tDivisible? " << "Yes";
+        }
+        else
+        {
+            std::cout << "\nRandom Number: \t" << rndNumber << "\tDivisible? " << "Nope";
+        }
+    }
+    std::cout << "\n";
+}
+
+// Program 1.9
+void Program9()
+{
+    std::cout << "==================== Program 1-9 ====================\n";
+    int choice = 0;
+
+    std::cout << "Colors of popcicles in my freezer\n";
+    std::cout << "1.. Red\n";
+    std::cout << "2.. Blue\n";
+    std::cout << "3.. Yellow\n";
+    std::cout << "4.. Orange\n";
+    std::cout << "5.. Purple\n";
+    std::cout << "6.. Green\n";
+    std::cout << "Which color would you like: ";
+    choice = ValidateInt(choice);
+    
+    switch (choice)
+    {
+    case 1:
+        {
+            std::cout << "\nCherry is always a great choice in flavors";
+            break;
+        }
+    case 2:
+        {
+            std::cout << "\nBlueberry is always an interesting flavor";
+            break;
+        }
+    case 3:
+        {
+            std::cout << "\nBanana is such a yummy taste";
+            break;
+        }
+    case 4:
+        {
+            std::cout << "\nOrange is makes the tongue a fun color";
+            break;
+        }
+    case 5:
+        {
+            std::cout << "\nGrape is not one of my favorite flavors.";
+            break;
+        }
+    case 6:
+        {
+            std::cout << "\nGreen Apple always a fan favorite.";
+            break;
+        }
+    default:
+        {
+            std::cout << "\nPlease choose a flavor I have in the freezer.";
+            break;
+        }
+    }
+
+    std::cout << "\n";
+}
+
+// Program 1-10
+void Program10()
+{
+    std::cout << "==================== Program 1-10 ====================\n";
+    int choice = 0;
+    int rndNumber = 0;
+    int number = 0;
+
+    srand(time(0));
+
+    std::cout << "\nPlease choose the level of difficulty to fight:";
+    std::cout << "\n1.. Easy";
+    std::cout << "\n2.. Medium";
+    std::cout << "\n3.. Hard";
+    std::cout << "\n4.. Extreme";
+    std::cout << "\nChoose your destiny! ";
+    number = ValidateInt(number);
+
+    switch (number)
+    {
+    case 1:
+	    {
+		    rndNumber = rand() % 10;
+		    std::cout << "\nYou will have to fight " << rndNumber << " monsters to get to the next level\n";
+            break;
+	    }
+    case 2:
+	    {
+		    rndNumber = rand() % 30;
+		    std::cout << "\nYou will have to fight " << rndNumber << " monsters to get to the next level\n";
+            break;
+	    }
+    case 3:
+	    {
+		    rndNumber = rand() % 50;
+		    std::cout << "\nYou will have to fight " << rndNumber << " monsters to get to the next level\n";
+            break;
+	    }
+    case 4:
+	    {
+		    rndNumber = rand() % 100;
+		    std::cout << "\nYou will have to fight " << rndNumber << " monsters, and hope to not die!\n";
+            break;
+	    }
+    default:
+	    {
+		    std::cout << "\nPlease choose the correct destiny and difficulty";
+            break;
+	    }
+
+    }
+}
+
+
+
 
 
