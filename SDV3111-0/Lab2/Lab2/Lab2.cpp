8f0c5ac2c1d07917c3f441c024ba92643d3467f5
Edward Roy Welborn <erwelborn@student.fullsail.edu>
2022-03-02 15:02:15 -0600

    Initial Lab 2 Setup. starting to work through the methods

---
+++ SDV3111-0/Lab2/Lab2/Lab2.cpp
@@ -0,0 +1,19 @@
+// Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Console.h"
+#include "pch.h"
+
+
+// Global Declarations
+unsigned int bitField();
+unsigned int bitF;
+unsigned int data;
+
+
+int main()
+{
+    
+}
+
