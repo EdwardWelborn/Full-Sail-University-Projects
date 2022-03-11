314d60ec13f70fd15fac95c865449ff898a0bc35
Edward Roy Welborn <erwelborn@student.fullsail.edu>
2022-03-10 22:02:16 -0600

    Lab3 Poniters completed and tested to make sure I get the right answers

---
+++ SDV3111-0/Lab3/Lab3/Car.h
@@ -0,0 +1,21 @@
+﻿// Car struct, and color definition enum
+
+enum EnumColorDefinition
+{
+    Red = 1,
+    Blue,
+    Green,
+    Yellow,
+    Purple,
+    Orange,
+    Black
+};
+
+//sets car definition class
+struct Car {
+    char make[32];
+    char model[32];
+    int year;
+    int mileage;
+    EnumColorDefinition color;
+};
